e0e7b47037d2bfefab72892f3061dee08aa94fae
Miko Hendro Cahyono <mikohendroc@gmail.com>
2017-10-31 14:22:13 +0700

    Update kesimpulan.php

--- kesimpulan.php
+++ kesimpulan.php
@@ -1,9 +1,405 @@
+<?php
+$g1 = "";
+$g2 = "";
+$g3 = "";
+$g4 = "";
+$g5 = "";
+$g6 = "";
+$g7 = "";
+$g8 = "";
+$g9 = "";
+$g10 = "";
+$g11 = "";
+$g12 = "";
+$g13 = "";
+$g14 = "";
+$g15 = "";
+$g16 = "";
+$g17 = "";
+$g18 = "";
+$g19 = "";
+$g20 = "";
+$g21 = "";
+$g22 = "";
+$g23 = "";
+$g24 = "";
+$g25 = "";
+$g26 = "";
+$g27 = "";
+$g28 = "";
+$g29 = "";
+$g30 = "";
+$g31 = "";
+$g32 = "";
+$g33 = "";
+$g34 = "";
+$g35 = "";
+$g36 = "";
+$g37 = "";
+$g38 = "";
+$g39 = "";
+$g40 = "";
+$g41 = "";
+$g42 = "";
+if (isset($_POST['g1'])) {
+    $g1 = 1;
+} else {
+    $g1 = 0;
+}
+
+if (isset($_POST['g2'])) {
+    $g2 = 1;
+} else {
+    $g2 = 0;
+}
+if (isset($_POST['g3'])) {
+    $g3 = 1;
+} else {
+    $g3 = 0;
+}
+if (isset($_POST['g4'])) {
+    $g4 = 1;
+} else {
+    $g4 = 0;
+}
+if (isset($_POST['g5'])) {
+    $g5 = 1;
+} else {
+    $g5 = 0;
+}
+if (isset($_POST['g6'])) {
+    $g6 = 1;
+} else {
+    $g6 = 0;
+}
+if (isset($_POST['g7'])) {
+    $g7 = 1;
+} else {
+    $g7 = 0;
+}
+if (isset($_POST['g8'])) {
+    $g8 = 1;
+} else {
+    $g8 = 0;
+}
+if (isset($_POST['g9'])) {
+    $g9 = 1;
+} else {
+    $g9 = 0;
+}
+if (isset($_POST['g10'])) {
+    $g10 = 1;
+} else {
+    $g10 = 0;
+}
+if (isset($_POST['g11'])) {
+    $g11 = 1;
+} else {
+    $g11 = 0;
+}
+if (isset($_POST['g12'])) {
+    $g12 = 1;
+} else {
+    $g12 = 0;
+}
+if (isset($_POST['g13'])) {
+    $g13 = 1;
+} else {
+    $g13 = 0;
+}
+if (isset($_POST['g14'])) {
+    $g14 = 1;
+} else {
+    $g14 = 0;
+}
+if (isset($_POST['g15'])) {
+    $g15 = 1;
+} else {
+    $g15 = 0;
+}
+if (isset($_POST['g16'])) {
+    $g16 = 1;
+} else {
+    $g16 = 0;
+}
+if (isset($_POST['g17'])) {
+    $g17 = 1;
+} else {
+    $g17 = 0;
+}
+if (isset($_POST['g18'])) {
+    $g18 = 1;
+} else {
+    $g18 = 0;
+}
+if (isset($_POST['g19'])) {
+    $g19 = 1;
+} else {
+    $g19 = 0;
+}
+if (isset($_POST['g20'])) {
+    $g20 = 1;
+} else {
+    $g20 = 0;
+}
+if (isset($_POST['g21'])) {
+    $g21 = 1;
+} else {
+    $g21 = 0;
+}
+if (isset($_POST['g22'])) {
+    $g22 = 1;
+} else {
+    $g22 = 0;
+}
+if (isset($_POST['g23'])) {
+    $g23 = 1;
+} else {
+    $g23 = 0;
+}
+if (isset($_POST['g24'])) {
+    $g24 = 1;
+} else {
+    $g24 = 0;
+}
+if (isset($_POST['g25'])) {
+    $g25 = 1;
+} else {
+    $g25 = 0;
+}
+if (isset($_POST['g26'])) {
+    $g26 = 1;
+} else {
+    $g26 = 0;
+}
+if (isset($_POST['g27'])) {
+    $g27 = 1;
+} else {
+    $g27 = 0;
+}
+if (isset($_POST['g28'])) {
+    $g28 = 1;
+} else {
+    $g28 = 0;
+}
+if (isset($_POST['g29'])) {
+    $g29 = 1;
+} else {
+    $g29 = 0;
+}
+if (isset($_POST['g30'])) {
+    $g30 = 1;
+} else {
+    $g30 = 0;
+}
+if (isset($_POST['g31'])) {
+    $g31 = 1;
+} else {
+    $g31 = 0;
+}
+if (isset($_POST['g32'])) {
+    $g32 = 1;
+} else {
+    $g32 = 0;
+}
+if (isset($_POST['g33'])) {
+    $g33 = 1;
+} else {
+    $g33 = 0;
+}
+if (isset($_POST['g34'])) {
+    $g34 = 1;
+} else {
+    $g34 = 0;
+}
+if (isset($_POST['g35'])) {
+    $g35 = 1;
+} else {
+    $g35 = 0;
+}
+if (isset($_POST['g36'])) {
+    $g36 = 1;
+} else {
+    $g36 = 0;
+}
+if (isset($_POST['g37'])) {
+    $g37 = 1;
+} else {
+    $g37 = 0;
+}
+if (isset($_POST['g38'])) {
+    $g38 = 1;
+} else {
+    $g38 = 0;
+}
+if (isset($_POST['g39'])) {
+    $g39 = 1;
+} else {
+    $g39 = 0;
+}
+if (isset($_POST['g40'])) {
+    $g40 = 1;
+} else {
+    $g40 = 0;
+}
+if (isset($_POST['g41'])) {
+    $g41 = 1;
+} else {
+    $g41 = 0;
+}
+if (isset($_POST['g42'])) {
+    $g42 = 1;
+} else {
+    $g42 = 0;
+}
+
+$kodeSolusi = array("S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "S10", "S11", "S12", "S13");
+$namaSolusi = array(
+    "Perbaikan pada software/OS (Flash/instal ulang OS)",
+    "Penggantian pada LCD",
+    "Penggantian pada Mainboard",
+    "Penggantian pada Speaker",
+    "Penggantian pada batrai",
+    "Penggantian pada kamera",
+    "Penggantian pada konektor charger/USB",
+    "Penggantian pada konektor simcard",
+    "Penggantian pada simcard",
+    "Penggantian pada konektor micro sd",
+    "Penggantian pada microphone",
+    "Penggantian pada antena signal",
+    "Penggantian pada lampu LED",
+);
+$gejalaKerusakan = array(
+    "Touch Screen",
+    "LCD",
+    "Mainboard",
+    "Speaker",
+    "Batrai",
+    "Kamera",
+    "Lampu Flash",
+    "Konektor Charger/USB",
+    "Konektor SIM Card",
+    "Konektor Micro SD",
+    "Microphone",
+    "Antena signal",
+    "LED"
+);
+$kerusakan = array();
+$solusi = array();
+
+//if 1
+if ($g1 == 1 && $g3 == 1) {
+    array_push($kerusakan, 0);
+    array_push($solusi, 0);
+}
+//if 2
+if ($g1 == 1 && $g2 == 1) {
+    array_push($kerusakan, 0);
+    array_push($solusi, 1);
+}
+//if 3
+if ($g4 == 1 && $g5 == 1) {
+    array_push($kerusakan, 1);
+    array_push($solusi, 1);
+}
+//if 4
+if ($g6 == 1 && $g7 == 1) {
+    array_push($kerusakan, 1);
+    array_push($solusi, 0);
+}
+//if 5
+if ($g10 == 1 && $g40 == 1 && $g42 == 1) {
+    array_push($kerusakan, 2);
+    array_push($solusi, 0);
+}
+//if 6
+if ($g8 == 1 && $g9 == 1) {
+    array_push($kerusakan, 2);
+    array_push($solusi, 2);
+}
+//if 7
+if ($g11 == 1 && $g12 == 1 && $g13 == 1) {
+    array_push($kerusakan, 3);
+    array_push($solusi, 3);
+}
+//if 8
+if ($g10 == 1 && $g16 == 1 && $g17 == 1) {
+    array_push($kerusakan, 4);
+    array_push($solusi, 0);
+}
+//if 9
+if ($g8 == 1 && $g14 == 1 && $g15 == 1) {
+    array_push($kerusakan, 4);
+    array_push($solusi, 4);
+}
+//if 10
+if ($g19 == 1 && $g20 == 1) {
+    array_push($kerusakan, 5);
+    array_push($solusi, 0);
+}
+//if 11
+if ($g18 == 1 && $g21 == 1 && $g22 == 1) {
+    array_push($kerusakan, 5);
+    array_push($solusi, 5);
+}
+//if 12
+if ($g23 == 1 && $g24 == 1) {
+    array_push($kerusakan, 6);
+    array_push($solusi, 2);
+}
+//if 13
+if ($g16 == 1 && $g26 == 1) {
+    array_push($kerusakan, 7);
+    array_push($solusi, 0);
+}
+//if 14
+if ($g15 == 1 && $g25 == 1) {
+    array_push($kerusakan, 7);
+    array_push($solusi, 6);
+}
+//if 15
+if ($g27 == 1 && $g28 == 1 && $g29 == 1) {
+    array_push($kerusakan, 8);
+    array_push($solusi, 7);
+}
+//if 16
+if ($g29 == 1 && $g41 == 1) {
+    array_push($kerusakan, 8);
+    array_push($solusi, 8);
+}
+//if 17
+if ($g30 == 1 && $g31 == 1) {
+    array_push($kerusakan, 9);
+    array_push($solusi, 9);
+}
+//if 18
+if ($g32 == 1 && $g33 == 1) {
+    array_push($kerusakan, 10);
+    array_push($solusi, 10);
+}
+//if 19
+if ($g34 == 1 && $g35 == 1 && $g36 == 1) {
+    array_push($kerusakan, 11);
+    array_push($solusi, 11);
+}
+//if 20
+if ($g37 == 1 && $g38 == 1 && $g39 == 1) {
+    array_push($kerusakan, 12);
+    array_push($solusi, 12);
+}
+
+//echo $kerusakan[0];
+//echo "jumlah kerusakan : " . count($kerusakan);
+//echo "<br>";
+//echo "jumlah solusi : " . count($solusi);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="widtd=device-widtd, initial-scale=1, maximum-scale=1, user-scalable=no">
-        <title>MyBiz Bootstrap Theme</title>
+        <title>Kesimpulan</title>
         <meta name="description" content="Free Bootstrap Theme by BootstrapMade.com">
         <meta name="keywords" content="free website templates, free bootstrap tdemes, free template, free bootstrap, free website template">
 
@@ -22,7 +418,7 @@
     <body id="top" data-spy="scroll">
         <!--top header-->
 
-        <?php include './header.php';?>
+        <?php include './header.php'; ?>
 
         <!--service-->
         <div id="service">
@@ -30,9 +426,44 @@
                 <div class="row">
                     <div class="col-xs-12 col-md-6 col-md-offset-3">
                         <div class="service-heading">
-                            <h2>KESIMPULAN : </h2> 
-                            <p>DARI HASIL ANALISIS DAPAT DISIMPULAN GEJALA ADALAH <i>Touch Screen bergerak sendiri</i> 
-                                DAN SOLUSINYA ADALAH <i>S2(Penggantian pada LCD)</i></p>
+                            <div class="col-sm-12">
+                                <h2>KESIMPULAN : </h2>
+                            </div>
+                            <hr>
+                            <hr>
+                            <div class="col-sm-12">
+                                <h5 class="text-warning">JENIS KERUSAKAN YANG DIALAMI : </h5>
+                                <h5 style="border: 1px solid black;padding: 20px;">
+                                    <?php
+                                    for ($i = 0; $i < count($solusi); $i++) {
+                                        echo ($i+1) .".". $gejalaKerusakan[$kerusakan[$i]] . "<br/><br/>";
+                                    }
+                                    ?>
+                                </h5>
+                            </div>
+                            <hr>
+                            <hr>
+                            <div class="col-sm-12">
+                                <h5 class="text-danger">SOLUSINYA ADALAH : </h5>
+                                <h5 style="border: 1px solid black;padding: 20px;">
+                                    <?php
+                                    for ($i = 0; $i < count($solusi); $i++) {
+                                        $nama_solusi = $namaSolusi[$solusi[$i]];
+                                        $kode_solusi = $kodeSolusi[$solusi[$i]];
+                                        echo ($i+1) .".". $nama_solusi . " / ($kode_solusi)" . "<br/><br/>";
+                                    }
+                                    ?>
+                                </h5>
+                            </div>
+                        </div>
+                        <div class="service-heading">
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <form action="dashboard.php">
+                                        <input type="submit" class="btn btn-success" value="Kembali">
+                                    </form>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>   	
